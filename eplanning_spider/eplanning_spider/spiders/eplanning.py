323e155d788cc75d16477d44c5b6f1abee8ed470
Goncalo Videira <gvideira@byzapplabs.com>
2022-06-23 19:23:02 +0100

    finished eplanning

--- eplanning_spider/eplanning_spider/spiders/eplanning.py
+++ eplanning_spider/eplanning_spider/spiders/eplanning.py
@@ -32,4 +32,37 @@ class EplanningSpider(scrapy.Spider):
             )
     
     def parse_pages(self, response):
-        pass
+        links = response.xpath('//td/a/@href').extract()
+        for link in links: 
+            yield Request(response.urljoin(link), callback=self.parse_items)
+
+        next_url = response.xpath('//*[@rel="next"]/@href').extract_first()
+        if next_url:
+            yield Request(response.urljoin(next_url), callback=self.parse_pages)
+    
+    def parse_items(self, response):
+        agent_btn = response.xpath('//*[@value="Agents" and @style="display: inline;  visibility: visible;"]')
+        if agent_btn:
+            name = response.xpath('//*[@title="Agent Details"]/table/tr[th="Name :"]/td/text()').extract_first()
+
+            address_first = response.xpath('//*[@title="Agent Details"]/table/tr[th="Address :"]/td/text()').extract()
+            address_second = response.xpath('//*[@title="Agent Details"]/table/tr[th="Address :"]/following-sibling::tr/td/text()').extract()[:3]
+            address = address_first + address_second
+
+            phone = response.xpath('//*[@title="Agent Details"]/table/tr[th="Phone :"]/td/text()').extract_first()
+            fax = response.xpath('//*[@title="Agent Details"]/table/tr[th="Fax :"]/td/text()').extract_first()
+            email = response.xpath('//*[@title="Agent Details"]/table/tr[th="e-mail :"]/td/a/text()').extract_first()
+
+            url = response.url
+
+            yield {
+                'Name': name,
+                'Address': address,
+                'Phone': phone,
+                'Fax': fax,
+                'Email': email,
+                'URL': url
+            }
+
+        else:
+            self.logger.info('Agents button not found on page. Passing invalid url!')
